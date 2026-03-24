eaf595cfd6c9ba7d53d07320f5c7662453a84db5
xdo <xavier.decool@5asolutions.com>
2026-03-24 15:07:03 +0100

    Maj

--- userinterface/screens.py
+++ userinterface/screens.py
@@ -1,8 +1,14 @@
 # Appel les Templates 
 
 from fastapi.templating import Jinja2Templates
-from fastapi import Request
 
 templates = Jinja2Templates(directory="userinterface/templates")
-def home_screen(request: Request):
-    return templates.TemplateResponse("home.html", {"request": request})
+
+#  désactive cache (clé du bug)
+templates.env.cache = {}
+
+def home_screen(request):
+    return templates.TemplateResponse(
+        "home.html",
+        {"request": request}
+    )
